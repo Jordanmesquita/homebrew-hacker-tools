868697eb64951511bbb972d6185c6dae3dd02d68
Jordan mesquita <jordanmesquita@MacBook-Air-3.local>
2026-02-17 00:05:04 -0500

    PERFECT v1.0.0

--- hacker-tools.rb
+++ hacker-tools.rb
@@ -4,9 +4,11 @@ class HackerTools < Formula
   url "https://github.com/Jordanmesquita/hacker-tools/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "4e0cc106c5d5755f7e43a5e844b75bf22f5d4a914249cbf5aa29e296be6894d5"
   license "MIT"
+
   def install
     bin.install Dir["bin/*"]
   end
+
   test do
     assert_match "01101000 01100101", shell_output("#{bin}/textbin hello")
   end
