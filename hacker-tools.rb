93d1290fdc286a03953e1680d834d0ac8b700679
Jordan mesquita <jordanmesquita@MacBook-Air-3.local>
2026-02-17 00:03:07 -0500

    Fix v1.0.0: GitHub URL + SHA256

--- hacker-tools.rb
+++ hacker-tools.rb
@@ -2,7 +2,7 @@ class HackerTools < Formula
   desc "Jordan's hacker toolbox: textbin + trans"
   homepage "https://github.com/Jordanmesquita/hacker-tools"
   url "https://github.com/Jordanmesquita/hacker-tools/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_REAL_HASH"
+  sha256 "4e0cc106c5d5755f7e43a5e844b75bf22f5d4a914249cbf5aa29e296be6894d5"
   license "MIT"
 
   def install
