7a2b22f7a835025f7230783696f3ebc771421a46
Jordan mesquita <jordanmesquita@MacBook-Air-3.local>
2026-02-17 00:12:34 -0500

    v1.0.1: simple-trans

--- hacker-tools.rb
+++ hacker-tools.rb
@@ -1,5 +1,5 @@
 class HackerTools < Formula
-  desc "Jordan's hacker toolbox: textbin + trans"
+  desc "Jordan's hacker toolbox: textbin + simple-trans"
   homepage "https://github.com/Jordanmesquita/hacker-tools"
   url "https://github.com/Jordanmesquita/hacker-tools/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "4e0cc106c5d5755f7e43a5e844b75bf22f5d4a914249cbf5aa29e296be6894d5"
@@ -10,6 +10,6 @@ class HackerTools < Formula
   end
 
   test do
-    assert_match "01101000 01100101", shell_output("#{bin}/textbin hello")
+    assert_match "hello", shell_output("#{bin}/simple-trans Hola")
   end
 end
